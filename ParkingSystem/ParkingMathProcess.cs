3bc57c89c05248e980d3e5c4e1d8c42a6467076b
ragalorio <ragalorio@itpi.com.ph>
2022-07-11 21:28:29 +0800

    FirstCommit

---
+++ ParkingSystem/ParkingMathProcess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingSystem
+{
+    public class ParkingMathProcess
+    {
+        public int AddProcess(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int SubProcess(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ ParkingSystemTests/ParkingMathProcessTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParkingSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingSystem.Tests
+{
+    [TestClass()]
+    public class ParkingMathProcessTests
+    {
+        [TestMethod()]
+        public void AddProcessTest()
+        {
+            ParkingMathProcess parkProcess = new ParkingMathProcess();
+            Assert.AreEqual(3, parkProcess.AddProcess(1, 2));
+        }
+
+        [TestMethod()]
+        public void SubProcessTest()
+        {
+            ParkingMathProcess parkProcess = new ParkingMathProcess();
+            Assert.AreEqual(3, parkProcess.SubProcess(1, 2));
+        }
+    }
+}
